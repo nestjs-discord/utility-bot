d201283551ad3d33e0c48a17a01eaad40577624e
Eros <129247554+erosdesire@users.noreply.github.com>
2023-09-04 14:57:21 +0330

    fix(config): can't dive on a non slice or map

--- core/config/config.go
+++ core/config/config.go
@@ -37,7 +37,7 @@ func GetGuildID() string {
 
 type Config struct {
 	Moderators []string            `mapstructure:"moderators" validate:"required,min=1,dive,min=1"`
-	Ratelimit  Ratelimit           `mapstructure:"ratelimit" validate:"required,dive"`
+	Ratelimit  Ratelimit           `mapstructure:"ratelimit" validate:"required"`
 	Commands   map[string]*Command `mapstructure:"commands" validate:"required,max-one-space-allowed,min=1,max=85,dive"`
 }
 
