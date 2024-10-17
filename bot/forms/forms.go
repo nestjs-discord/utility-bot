01156869e63d4faa73863243b29c64f9b0b84fdd
Eros <129247554+erosdesire@users.noreply.github.com>
2024-10-17 09:18:48 +0330

    refactor

--- bot/forms/forms.go
+++ bot/forms/forms.go
@@ -30,32 +30,54 @@ func NewForms(cfg yaml.Forms, session *dgo.Session) (*Forms, error) {
 		return nil, err
 	}
 
-	// ----------------
-	// TODO: refactor the lines below (extract method)
-	// ----------------
+	err = f.synchronizeOpenModalButtons(session)
+	if err != nil {
+		return nil, err
+	}
 
-	limit := 1
-	beforeId := ""
-	afterId := ""
-	aroundId := ""
+	return f, nil
+}
 
-	for formId, form := range cfg {
-		messages, err := session.ChannelMessages(form.ChannelId, limit, beforeId, afterId, aroundId)
+func (f *Forms) synchronizeOpenModalButtons(session *dgo.Session) error {
+	for formId, form := range f.cfg {
+		message, err := f.getLastChannelMessage(session, form.ChannelId)
 		if err != nil {
-			return nil, fmt.Errorf("unable to get channel messages: %s", err)
+			return err
 		}
 
-		if len(messages) != 0 && f.doesHaveButtonComponentWithLabel(messages[0], form.ButtonLabel) {
+		if message == nil {
+			continue
+		}
+
+		if f.doesHaveButtonComponentWithLabel(message, form.ButtonLabel) {
 			continue
 		}
 
 		label := form.ButtonLabel
 		if err = f.sendFormButton(session, form.ChannelId, formId, label); err != nil {
-			return nil, fmt.Errorf("unable to send form button: %s", err)
+			return fmt.Errorf("unable to send form button: %s", err)
 		}
 	}
 
-	return f, nil
+	return nil
+}
+
+func (f *Forms) getLastChannelMessage(session *dgo.Session, channelId string) (*dgo.Message, error) {
+	limit := 1
+	beforeId := ""
+	afterId := ""
+	aroundId := ""
+
+	messages, err := session.ChannelMessages(channelId, limit, beforeId, afterId, aroundId)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get channel messages: %s", err)
+	}
+
+	if len(messages) == 0 {
+		return nil, nil
+	}
+
+	return messages[0], nil
 }
 
 func (f *Forms) getFormById(id string) (*yaml.Form, error) {
